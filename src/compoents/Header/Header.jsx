101feae66d9fe6ae06b2d634ffafbeb30bad6a27
leejungho9 <9wjdgh9@naver.com>
2023-05-13 03:34:09 +0900

    design : Header Component 레이아웃 및 스타일 지정
    - Header Logo 생성
    - 실시간 예약 버튼 생성
    
    Related to : #2

---
+++ src/compoents/Header/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import { LineFlex } from '../../styles/GlobalStyle';
+import logo from '../../assets/images/logo.png';
+import reserLogo from '../../assets/images/reserLogo.png';
+const HeaderContainer = styled.header`
+  height: 150px;
+`;
+
+const HeaderWrapper = styled.div`
+  width: 1480px;
+  height: 100%;
+  box-sizing: border-box;
+  ${LineFlex}
+  justify-content: space-between;
+  margin: 0 auto;
+`;
+
+const Logo = styled.img``;
+const ReserLogo = styled.img`
+  width: 15px;
+  height: 15px;
+  margin-right: 10px;
+`;
+
+// ! 나중에 Link로 교체
+const ReserveButton = styled.div`
+  font-size: 15px;
+  ${LineFlex}
+  cursor: pointer;
+`;
+
+const ReserSpan = styled.span``;
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderWrapper>
+        <Logo src={logo} alt=" 임시로고" />
+        <ReserveButton>
+          <ReserLogo src={reserLogo} alt="예약로고" />
+          <ReserSpan>실시간 예약</ReserSpan>
+        </ReserveButton>
+      </HeaderWrapper>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
