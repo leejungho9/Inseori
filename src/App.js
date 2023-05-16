12520c37e1fe29f8e8ec3eaa14fa0defc79bb0cd
leejungho9 <9wjdgh9@naver.com>
2023-05-17 00:50:23 +0900

    feat : router 연결 및 스크롤 이벤트 기능 구현
    - RoomA router 연결
    - 헤더 관련 스크롤 이벤트 기능 구현( footer의 거리에 따른 position 변경)
    
    Related to : #10

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import Main from './pages/Main/Main';
-import GlobalStyle from './styles/GlobalStyle';
+import Footer from './compoents/Footer/Footer';
+import Header from './compoents/Header/Header';
+import RoomA from './pages/Rooms/RoomA';
+import { useEffect } from 'react';
 
 function App() {
+  const { pathname } = useLocation();
+  window.scrollTo(0, 0);
+
+  //! 헤더 스크롤 이벤트
+  useEffect(() => {
+    const handleScroll = () => {
+      const headerEl = document.querySelector('.header');
+      const footerEl = document.querySelector('.footer');
+      if (pathname !== '/' && headerEl && footerEl) {
+        const footerTop = footerEl.offsetTop; //! 푸터 최상단 위치
+        const windowHeight = window.innerHeight; // ! 브라우저 높이
+        const offsetMenu = footerTop - windowHeight; // ! 푸터 위치 - 브라우저 높이
+        if (window.innerWidth > 1024) {
+          if (window.pageYOffset >= offsetMenu) {
+            headerEl.style.position = 'absolute';
+            headerEl.style.top = offsetMenu + 'px';
+          } else {
+            headerEl.style.position = 'fixed';
+            headerEl.style.top = '0';
+          }
+        }
+      } else {
+        headerEl.style.position = 'absolute';
+        headerEl.style.top = '0';
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+  }, [pathname]);
+
   return (
     <>
-      <GlobalStyle />
-      <Main />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/RoomA" element={<RoomA />}></Route>
+      </Routes>
+      <Footer />
     </>
   );
 }
