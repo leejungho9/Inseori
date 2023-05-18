9ab690b24c75d9e57d4420a23071fe8d2d914198
leejungho9 <9wjdgh9@naver.com>
2023-05-18 21:50:07 +0900

    desing : css 변수 적용

--- src/compoents/Button/ReserveButton.jsx
+++ src/compoents/Button/ReserveButton.jsx
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { RowFlex } from '../../styles/GlobalStyle';
 const ReserveBtn = styled(Link)`
-  border: 1px solid var(--black);
+  border: 2px solid var(--gray);
   font-weight: bold;
   width: 120px;
   height: 45px;
   border-radius: 30px;
   ${RowFlex}
   align-items: center;
-  font-size: var(--text-size-20);
+  font-size: var(--text-size-18);
+  color: var(--gray);
 `;
 function ReserveButton({ children, url }) {
   return <ReserveBtn to={url}>{children}</ReserveBtn>;
