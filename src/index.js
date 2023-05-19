18a37ce800ebb8c37e88ee13ccd1627944c17a7f
leejungho9 <9wjdgh9@naver.com>
2023-05-19 17:00:15 +0900

    feat : 최상단 자동스크롤 기능 구현
    - react-router 페이지 이동시 최상단으로 자동 스크롤 기능 구현

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
+import ScrollToTop from './hooks/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <GlobalStyle />
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
 );
