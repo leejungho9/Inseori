2ac97b4f3de8388e1500f9c8fb15daeb23dcb20b
leejungho9 <9wjdgh9@naver.com>
2023-05-24 21:58:33 +0900

    design : main 반응형 스타일 추가

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -15,21 +15,35 @@ const MainWrpper = styled.figure`
   width: calc(100% - 435px);
   position: relative;
   margin-top: 150px;
+
+  @media screen and (max-width: 991px) {
+    width: 100%;
+  }
 `;
 
 const MainIntroWrapper = styled.section`
   width: 100%;
   display: flex;
   padding: 150px 0 150px;
+  @media screen and (max-width: 991px) {
+    padding: 100px 0 155px;
+  }
 `;
 const MainIntroBox = styled.div`
   width: 510px;
   text-align: center;
   line-height: 2;
   margin: 0 auto;
+  @media screen and (max-width: 991px) {
+    width: 450px;
+  }
 `;
-const MainIntroSpan = styled.span`
+const MainIntroP = styled.span`
   font-size: var(--text-size-15);
+  @media screen and (max-width: 991px) {
+    font-size: var(--text-size-20);
+    word-break: keep-all;
+  }
 `;
 
 const MainGalleryCarouselWrapper = styled.figure`
@@ -37,6 +51,9 @@ const MainGalleryCarouselWrapper = styled.figure`
   width: 100%;
   margin-bottom: 150px;
   position: relative;
+  @media screen and (max-width: 991px) {
+    margin-bottom: 120px;
+  }
 `;
 
 const Main = forwardRef((props, mainTopRef) => {
@@ -46,12 +63,12 @@ const Main = forwardRef((props, mainTopRef) => {
         <MainBannerCarousel />
         <MainIntroWrapper>
           <MainIntroBox>
-            <MainIntroSpan>
+            <MainIntroP>
               인서리공원은 광양시에서 도시재생 프로그램으로 리노베이션한 크고
               작은 14채 공간을 활용하여 보다 많은 사람들이 더 쉽게 그리고 더
               가까이 예술을 접하고 향유 할 수 있기를 바라는 기대로 만들어진
               복합문화공간입니다.
-            </MainIntroSpan>
+            </MainIntroP>
           </MainIntroBox>
         </MainIntroWrapper>
         <MainGalleryCarouselWrapper>
