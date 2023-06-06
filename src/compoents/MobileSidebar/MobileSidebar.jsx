0081f350d31d7498d3440309047c58a72cdedfff
leejungho9 <9wjdgh9@naver.com>
2023-06-06 18:20:18 +0900

    design : 모바일 사이드바 레이아웃 및 스타일 작업
    - 사이드바 스타일 작업
    - x 버튼 클릭시 사이드바 없어지도록 기능 구현
    
    Related to : #13

---
+++ src/compoents/MobileSidebar/MobileSidebar.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from 'styled-components';
+import reserLogo from '../../assets/images/reserLogo.png';
+import { IoMdClose } from 'react-icons/io';
+import MobileNav from '../Nav/MobileNav';
+
+const SidebarWrapper = styled.div`
+  width: 100%;
+  background-color: #f6f6f6;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  z-index: 100;
+  transition: all 0.8s ease 0s;
+  pointer-events: ${({ showSidebar }) => (showSidebar ? 'all' : 'none')};
+  right: ${({ showSidebar }) => (showSidebar ? '0px' : '-1000px')};
+  visibility: ${({ showSidebar }) => (showSidebar ? 'visible' : 'hidden')};
+`;
+
+const SidebarContainer = styled.div`
+  padding-top: 25px;
+  padding-left: 35px;
+`;
+const CloseIconBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 80px;
+  @media screen and (max-width: 991px) {
+    padding-right: 50px;
+  }
+  @media screen and (max-width: 500px) {
+    padding-right: 20px;
+  }
+`;
+const CloseIcon = styled(IoMdClose)`
+  font-size: 25px;
+  cursor: pointer;
+`;
+const ReserveLink = styled.a`
+  display: flex;
+  padding-left: 28px;
+`;
+const ReserveLogo = styled.img`
+  width: 22px;
+  height: 22px;
+  margin-right: 15px;
+  cursor: pointer;
+`;
+
+const ReserveSpan = styled.span`
+  cursor: pointer;
+  font-size: var(--text-size-18);
+  font-weight: bold;
+  color: #515151;
+`;
+
+const MobileNavWrapper = styled.div`
+  margin-top: 70px;
+`;
+
+function MobileSidebar({ setShowSidebar, showSidebar }) {
+  return (
+    <SidebarWrapper showSidebar={showSidebar}>
+      <SidebarContainer>
+        <CloseIconBox>
+          <CloseIcon onClick={() => setShowSidebar(!showSidebar)} />
+        </CloseIconBox>
+        <ReserveLink
+          target="_blank"
+          href="https://booking.naver.com/booking/3/bizes/802107"
+          rel="noreferrer"
+        >
+          <ReserveLogo src={reserLogo} alt="예약로고" />
+          <ReserveSpan>실시간 예약</ReserveSpan>
+        </ReserveLink>
+        <MobileNavWrapper>
+          <MobileNav />
+        </MobileNavWrapper>
+      </SidebarContainer>
+    </SidebarWrapper>
+  );
+}
+
+export default MobileSidebar;
