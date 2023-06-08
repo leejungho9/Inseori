eb3b3ba2d4fb3eaf907b48b5c6065ed7ef8fb329
leejungho9 <9wjdgh9@naver.com>
2023-06-08 13:04:29 +0900

    design : Searchbar Component 생성
    - 재사용 가능성을 고려한 검색바 컴포넌트 생성

---
+++ src/compoents/Searchbar/Searchbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+const SearchbarInput = styled.input`
+  width: 100%;
+  height: 100%;
+  background-color: #f6f6f6;
+  border-radius: 28px;
+  outline: none;
+  border: none;
+  padding: 5px 20px;
+  padding-right: ${({ paddingRight }) => paddingRight && paddingRight + 'px'};
+  font-size: var(--text-size-14);
+  font-family: 'PretendardRegular';
+`;
+
+const Searchbar = ({ paddingRight }) => {
+  return (
+    <SearchbarInput
+      placeholder="검색어를 입력해주세요."
+      paddingRight={paddingRight}
+    />
+  );
+};
+
+export default Searchbar;
