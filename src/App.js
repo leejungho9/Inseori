67d8dba3b7e3e13a9f876971f69b4eccb20e573f
leejungho9 <9wjdgh9@naver.com>
2023-06-08 23:28:22 +0900

    rename : import 파일 절대경로로 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
-import Footer from './compoents/Footer/Footer';
-import Header from './compoents/Header/Header';
+import Footer from './components/Layout/Footer/Footer';
+import Header from 'components/Layout/Header/Header';
 import { useEffect, useRef, useState } from 'react';
-import RoomA from './pages/RoomA';
-import Main from './pages/Main';
-import OneDayCalss from './pages/OneDayCalss';
-import OneDayClassDetail from './pages/OneDayClassDetail';
-import ArtShop from './pages/ArtShop';
+import RoomA from 'pages/RoomA';
+import Main from 'pages/Main';
+import OneDayCalss from 'pages/OneDayCalss';
+import OneDayClassDetail from 'pages/OneDayClassDetail';
+import ArtShop from 'pages/ArtShop';
 
 function App() {
   const [topHeaderVisible, setTopHeaderVisible] = useState(true);
@@ -19,12 +19,9 @@ function App() {
     headerRef: useRef(null),
     topHeaderRef: useRef(null),
   };
-  //! 헤더 스크롤 이벤트
-
+  //! 상단 헤더와 오른쪽 네브바 스크롤 이벤트
   useEffect(() => {
     const handleScroll = () => {
-      // let scrollY = window.scrollY + 1;
-
       const footerTop = footerRef.current.offsetTop; //! 푸터 최상단 위치
       const windowHeight = window.innerHeight; // ! 브라우저 높이
       const footerOffset = footerTop - windowHeight; // ! 푸터 위치 - 브라우저 높이
@@ -38,10 +35,10 @@ function App() {
           }
         }
       }
-      // ! 상단 헤더 숨기는 이벤트
 
+      // ! 상단 헤더 숨기는 이벤트
       if (refs.headerRef) {
-        //! 안먹어서 일단 150px 값으로 적어둠
+        //! 아래 코드가 작동 안해서 임시로 80px 값으로 적어둠
         // if (window.scrollY > refs.topHeaderRef.current.offsetHeight) {
         if (window.scrollY < 80) {
           if (window.innerWidth > 991) {
