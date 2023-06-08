b9b20386003504f869683e0f6991ff0b26ecc603
leejungho9 <9wjdgh9@naver.com>
2023-06-08 13:07:07 +0900

    design : 원데이클래스 Navbar 생성
    - 기존 Status 제거
    - Navbar 생성

--- src/pages/OneDayClass/OneDayCalss.jsx
+++ src/pages/OneDayClass/OneDayCalss.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import oneday1 from '../../assets/images/OneDayClass/oneday1.jpg';
 import oneday2 from '../../assets/images/OneDayClass/oneday2.jpg';
 import oneday3 from '../../assets/images/OneDayClass/oneday3.jpg';
-import { Flex } from '../../styles/GlobalStyle';
 import { Link } from 'react-router-dom';
+import Searchbar from '../../compoents/Searchbar/Searchbar';
+import { GoSearch } from 'react-icons/go';
 
 const OneDayCalssContainer = styled.main`
-  /* position: relative; */
   overflow: hidden;
 `;
 const OneDayCalssWrapper = styled.figure`
@@ -36,6 +36,7 @@ const MobileTitle = styled.h1`
 
 const OneDayCalssContentWrapper = styled.section`
   width: 100%;
+  padding-top: 100px;
   padding-right: 100px;
   display: grid;
   row-gap: 120px;
@@ -55,26 +56,6 @@ const OneDayClassContentBox = styled(Link)``;
 const OneDayClassImageBox = styled.div`
   position: relative;
   cursor: pointer;
-`;
-
-const OneDayClassTextImage = styled.div`
-  position: absolute;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  opacity: 50%;
-  background-color: #212121;
-  border-radius: 10px;
-  ${Flex}
-`;
-
-const OneDayClassText = styled.span`
-  font-size: var(--text-size-25);
-  color: var(--white);
-  font-weight: bold;
-  letter-spacing: 1.5px;
-  cursor: pointer;
-  text-align: center;
 `;
 const OneDayClassImage = styled.img`
   width: 100%;
@@ -100,15 +81,6 @@ const OneDayClassTitleBox = styled.div``;
 const OneDayClassInfoTitle = styled.h2`
   color: var(--dark);
   font-size: var(--text-size-15);
-`;
-const OneDayClassStatusBox = styled.div`
-  @media screen and (max-width: 500px) {
-    display: none;
-  }
-`;
-const OneDayClassStatus = styled.h2`
-  font-size: var(--text-size-15);
-  color: var(--text-point-color);
 `;
 const OneDayClassInfoBody = styled.div`
   padding: 5px 0;
@@ -144,11 +116,98 @@ const OneDayClassPrice = styled.h2`
   font-weight: bold;
 `;
 
+//! Nav
+const OneDayClassNav = styled.div`
+  height: 52px;
+  margin-right: 100px;
+  display: flex;
+  justify-content: space-between;
+`;
+const OneDayClassNavUl = styled.ul`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+const OneDayClassNavLi = styled.li`
+  font-size: var(--text-size-18);
+`;
+
+const OneDayClassNavLink = styled(Link)`
+  margin-right: 40px;
+  font-weight: ${({ active }) => (active ? 'bold' : 'normal')};
+  cursor: pointer;
+`;
+
+const OneDayClassSearchBox = styled.div`
+  position: relative;
+  width: 428px;
+`;
+const SearchIcon = styled(GoSearch)`
+  position: absolute;
+  right: 25px;
+  top: 15px;
+  font-size: 20px;
+  cursor: pointer;
+`;
 const OneDayCalss = () => {
+  const [classNavMenu, setClassNavMenu] = useState('전체');
+
+  const clickNavMenuHandle = (event) => {
+    setClassNavMenu(event.target.innerText);
+  };
+
   return (
     <OneDayCalssContainer>
       <OneDayCalssWrapper>
         <MobileTitle>원데이클래스</MobileTitle>
+        <OneDayClassNav>
+          <OneDayClassNavUl>
+            <OneDayClassNavLi>
+              <OneDayClassNavLink
+                onClick={clickNavMenuHandle}
+                active={classNavMenu === '아트포스터'}
+              >
+                아트포스터
+              </OneDayClassNavLink>
+            </OneDayClassNavLi>
+            <OneDayClassNavLi>
+              <OneDayClassNavLink
+                onClick={clickNavMenuHandle}
+                active={classNavMenu === '에디션판화'}
+              >
+                에디션판화
+              </OneDayClassNavLink>
+            </OneDayClassNavLi>
+            <OneDayClassNavLi>
+              <OneDayClassNavLink
+                onClick={clickNavMenuHandle}
+                active={classNavMenu === '굿즈/오브제'}
+              >
+                굿즈/오브제
+              </OneDayClassNavLink>
+            </OneDayClassNavLi>
+            <OneDayClassNavLi>
+              <OneDayClassNavLink
+                onClick={clickNavMenuHandle}
+                active={classNavMenu === '기타'}
+              >
+                기타
+              </OneDayClassNavLink>
+            </OneDayClassNavLi>
+            <OneDayClassNavLi>
+              <OneDayClassNavLink
+                onClick={clickNavMenuHandle}
+                active={classNavMenu === '전체'}
+              >
+                전체
+              </OneDayClassNavLink>
+            </OneDayClassNavLi>
+          </OneDayClassNavUl>
+          <OneDayClassSearchBox>
+            <SearchIcon />
+            <Searchbar paddingRight={50} />
+          </OneDayClassSearchBox>
+        </OneDayClassNav>
         <OneDayCalssContentWrapper>
           {/* ! 반복 시작 */}
           <OneDayClassContentBox to="/onedayclass/1">
@@ -163,9 +222,6 @@ const OneDayCalss = () => {
                 <OneDayClassTitleBox>
                   <OneDayClassInfoTitle>라탄조명 클래스</OneDayClassInfoTitle>
                 </OneDayClassTitleBox>
-                <OneDayClassStatusBox>
-                  <OneDayClassStatus>모집중</OneDayClassStatus>
-                </OneDayClassStatusBox>
               </OneDayClassInfoHead>
               <OneDayClassInfoBody>
                 <OneDayClassDateBox>
@@ -198,9 +254,6 @@ const OneDayCalss = () => {
                 <OneDayClassTitleBox>
                   <OneDayClassInfoTitle>라탄조명 클래스</OneDayClassInfoTitle>
                 </OneDayClassTitleBox>
-                <OneDayClassStatusBox>
-                  <OneDayClassStatus>모집중</OneDayClassStatus>
-                </OneDayClassStatusBox>
               </OneDayClassInfoHead>
               <OneDayClassInfoBody>
                 <OneDayClassDateBox>
@@ -221,30 +274,23 @@ const OneDayCalss = () => {
           </OneDayClassContentBox>
           {/* ! 반복 끝 */}
           {/* ! 반복 시작 */}
-          <OneDayClassContentBox>
+          <OneDayClassContentBox to="/onedayclass/1">
             <OneDayClassImageBox>
               <OneDayClassImage
                 src={oneday2}
                 alt="원데이클래스 이미지"
               ></OneDayClassImage>
-              {/* class.status === 'coming soon' */}
-              <OneDayClassTextImage>
-                <OneDayClassText>Coming Soon</OneDayClassText>
-              </OneDayClassTextImage>
             </OneDayClassImageBox>
             <OneDayClassInfoBox>
               <OneDayClassInfoHead>
                 <OneDayClassTitleBox>
                   <OneDayClassInfoTitle>라탄조명 클래스</OneDayClassInfoTitle>
                 </OneDayClassTitleBox>
-                <OneDayClassStatusBox>
-                  <OneDayClassStatus></OneDayClassStatus>
-                </OneDayClassStatusBox>
               </OneDayClassInfoHead>
               <OneDayClassInfoBody>
                 <OneDayClassDateBox>
                   <OneDayClassDate>
-                    2023.5.13(토)<span>/</span> 11:00~13:00
+                    2023.5.13(토) <span>/</span> 11:00~13:00
                   </OneDayClassDate>
                 </OneDayClassDateBox>
                 <OneDayClassPersonnelBox>
@@ -260,26 +306,82 @@ const OneDayCalss = () => {
           </OneDayClassContentBox>
           {/* ! 반복 끝 */}
           {/* ! 반복 시작 */}
-          <OneDayClassContentBox>
+          <OneDayClassContentBox to="/onedayclass/1">
             <OneDayClassImageBox>
               <OneDayClassImage
                 src={oneday3}
                 alt="원데이클래스 이미지"
               ></OneDayClassImage>
-              {/* ! Status close */}
-              {/* class.status === 'clsoe' */}
-              <OneDayClassTextImage>
-                <OneDayClassText>Closed</OneDayClassText>
-              </OneDayClassTextImage>
             </OneDayClassImageBox>
             <OneDayClassInfoBox>
               <OneDayClassInfoHead>
                 <OneDayClassTitleBox>
                   <OneDayClassInfoTitle>라탄조명 클래스</OneDayClassInfoTitle>
                 </OneDayClassTitleBox>
-                <OneDayClassStatusBox>
-                  <OneDayClassStatus></OneDayClassStatus>
-                </OneDayClassStatusBox>
+              </OneDayClassInfoHead>
+              <OneDayClassInfoBody>
+                <OneDayClassDateBox>
+                  <OneDayClassDate>
+                    2023.5.13(토) <span>/</span> 11:00~13:00
+                  </OneDayClassDate>
+                </OneDayClassDateBox>
+                <OneDayClassPersonnelBox>
+                  <OneDayClassPersonnel>인원 4-8명</OneDayClassPersonnel>
+                </OneDayClassPersonnelBox>
+              </OneDayClassInfoBody>
+              <OneDayClassInfoFoot>
+                <OneDayClassPriceBox>
+                  <OneDayClassPrice>45,000</OneDayClassPrice>
+                </OneDayClassPriceBox>
+              </OneDayClassInfoFoot>
+            </OneDayClassInfoBox>
+          </OneDayClassContentBox>
+          {/* ! 반복 끝 */}
+          {/* ! 반복 시작 */}
+          <OneDayClassContentBox to="/onedayclass/1">
+            <OneDayClassImageBox>
+              <OneDayClassImage
+                src={oneday2}
+                alt="원데이클래스 이미지"
+              ></OneDayClassImage>
+            </OneDayClassImageBox>
+            <OneDayClassInfoBox>
+              <OneDayClassInfoHead>
+                <OneDayClassTitleBox>
+                  <OneDayClassInfoTitle>라탄조명 클래스</OneDayClassInfoTitle>
+                </OneDayClassTitleBox>
+              </OneDayClassInfoHead>
+              <OneDayClassInfoBody>
+                <OneDayClassDateBox>
+                  <OneDayClassDate>
+                    2023.5.13(토) <span>/</span> 11:00~13:00
+                  </OneDayClassDate>
+                </OneDayClassDateBox>
+                <OneDayClassPersonnelBox>
+                  <OneDayClassPersonnel>인원 4-8명</OneDayClassPersonnel>
+                </OneDayClassPersonnelBox>
+              </OneDayClassInfoBody>
+              <OneDayClassInfoFoot>
+                <OneDayClassPriceBox>
+                  <OneDayClassPrice>45,000</OneDayClassPrice>
+                </OneDayClassPriceBox>
+              </OneDayClassInfoFoot>
+            </OneDayClassInfoBox>
+          </OneDayClassContentBox>
+          {/* ! 반복 끝 */}
+          {/* ! 반복 시작 */}
+          <OneDayClassContentBox to="/onedayclass/1">
+            <OneDayClassImageBox>
+              <OneDayClassImage
+                src={oneday1}
+                alt="원데이클래스 이미지"
+              ></OneDayClassImage>
+            </OneDayClassImageBox>
+            <OneDayClassInfoBox>
+              <OneDayClassInfoHead>
+                <OneDayClassTitleBox>
+                  <OneDayClassInfoTitle>라탄조명 클래스</OneDayClassInfoTitle>
+                </OneDayClassTitleBox>
               </OneDayClassInfoHead>
               <OneDayClassInfoBody>
                 <OneDayClassDateBox>
