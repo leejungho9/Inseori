c4e18217db82cf8bfe2c8fe6bfb12638c8c8ae2a
leejungho9 <9wjdgh9@naver.com>
2023-06-09 17:44:56 +0900

    update : ReserveButton link props 추가
    - 겉모습은 같아도 페이지를 이동하는 button 과 데이터를 보내는 submit
      버튼을 나누기 위한 link props 추가
    
    - optional 값으로 기본값은 true
    
    Related to : #11

--- src/components/Common/Button/ReserveButton.jsx
+++ src/components/Common/Button/ReserveButton.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { RowFlex } from 'styles/GlobalStyle';
-const ReserveBtn = styled(Link)`
+
+const ReserveLinkBtn = styled(Link)`
   border: 1px solid var(--gray);
   font-weight: bold;
   width: 100%;
@@ -13,8 +14,25 @@ const ReserveBtn = styled(Link)`
   font-size: var(--text-size-16);
   color: var(--gray);
 `;
-function ReserveButton({ children, url }) {
-  return <ReserveBtn to={url}>{children}</ReserveBtn>;
+
+const ReserveSubmitBtn = styled.button`
+  border: 1px solid var(--gray);
+  border-radius: 30px;
+  background-color: white;
+  font-weight: bold;
+  width: 100%;
+  height: 40px;
+  ${RowFlex}
+  align-items: center;
+  font-size: var(--text-size-16);
+  color: var(--gray);
+`;
+function ReserveButton({ children, url, link = true }) {
+  return link ? (
+    <ReserveLinkBtn to={url}>{children}</ReserveLinkBtn>
+  ) : (
+    <ReserveSubmitBtn>{children}</ReserveSubmitBtn>
+  );
 }
 
 export default ReserveButton;
