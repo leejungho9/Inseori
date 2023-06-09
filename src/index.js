8429aff93e7aca764c3844e89cb6f335f4454116
leejungho9 <9wjdgh9@naver.com>
2023-06-09 17:40:30 +0900

    remove : Router v6 업데이트로 인한 코드제거

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import GlobalStyle from './styles/GlobalStyle';
-import ScrollToTop from './hooks/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <GlobalStyle />
-    <ScrollToTop />
-    <App />
-  </BrowserRouter>,
-);
+root.render(<App />);
