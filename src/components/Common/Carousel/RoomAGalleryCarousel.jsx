66eaf926dbfc4424fe3db06b9440b17bc5de7e69
leejungho9 <9wjdgh9@naver.com>
2023-06-11 00:42:58 +0900

    design : slick-carousel css 수정
    - 홰경당 갤러리 캐러셀 css 수정

--- src/components/Common/Carousel/RoomAGalleryCarousel.jsx
+++ src/components/Common/Carousel/RoomAGalleryCarousel.jsx
@@ -10,32 +10,27 @@ import RoomA_4 from 'assets/images/RoomA/RoomB_4.jpg';
 import RoomA_5 from 'assets/images/RoomA/RoomB_5.jpg';
 
 const SliderContainer = styled.section`
-  .slick-slider {
-  }
-  .slick-list {
+  .slick-slide {
+    width: 315px !important;
   }
   .slick-track {
-  }
-  .slick-slide {
+    height: 300px;
   }
 `;
 const MainImgBox = styled.div`
   outline: none;
-  height: 277px !important;
-  width: 277px !important;
 `;
 const MainImg = styled.img`
-  height: 277px !important;
-  width: 277px !important;
+  height: 300px !important;
+  width: 300px !important;
   object-fit: cover;
 `;
 
 function RoomAGalleryCarousel() {
   const settings = {
     infinite: true, // 슬라이드 갯수에 상관없이 무한반복
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 2000,
-    centerMode: true,
     slidesToShow: 1, //보여지는 슬라이드 갯수
     slidesToScroll: 1, //스크롤 넘겼을때 넘어가는 슬라이드 갯수
     draggable: true,
