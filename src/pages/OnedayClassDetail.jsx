f11b27ce7999e39d8c1216227d94c01f4fccf459
leejungho9 <9wjdgh9@naver.com>
2023-06-11 19:03:41 +0900

    design : 반응형 미디어쿼리 추가
    - 원데이클래스 상세페이지 미디어쿼리 추가

--- src/pages/OnedayClassDetail.jsx
+++ src/pages/OnedayClassDetail.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import classBanner from 'assets/images/OneDayClassDetail/onedayclassDetail.png';
 import ReservationForm from 'components/Common/Form/ReservationForm';
+import onedayCalssImage1 from 'assets/images/OneDayClassDetail/onedayClassImage1.png';
+import onedayCalssImage2 from 'assets/images/OneDayClassDetail/onedayClassImage2.png';
 
 const OnedayClassDetialContainer = styled.main`
   overflow: hidden;
@@ -9,20 +11,37 @@ const OnedayClassDetialContainer = styled.main`
 
 const OnedayClassDetailWrapper = styled.section`
   float: right;
+  //! 전체너비 - 네브바 너비
   width: calc(100% - 435px);
   position: relative;
   margin-top: 150px;
   padding-bottom: 130px;
   padding-right: 100px;
+
+  @media screen and (max-width: 991px) {
+    width: 100%;
+    margin-top: 70px;
+    padding-bottom: 75px;
+    padding-right: 0;
+  }
 `;
 
 const OnedayClassDetailTitle = styled.h1`
+  font-family: 'PretendardBold';
   font-size: var(--text-size-18);
   font-weight: bold;
+
+  @media screen and (max-width: 991px) {
+    margin: 30px 25px;
+    font-size: 22px;
+  }
 `;
 
 const ReservationWrapper = styled.article`
   width: 100%;
+  @media screen and (max-width: 991px) {
+    padding: 0 25px;
+  }
 `;
 
 const ReservationBanner = styled.figure`
@@ -34,26 +53,74 @@ const ReservationBanner = styled.figure`
   height: 300px;
   border-radius: 20px;
   margin-top: 30px;
+
+  @media screen and (max-width: 991px) {
+    width: 100%;
+    height: 430px;
+  }
 `;
 
 const ReservationFormBox = styled.div`
-  margin: 30px 0;
+  margin: 45px 0px 0px;
 `;
 
 const ReservationFormTitle = styled.h2`
+  font-family: 'PretendardBold';
   font-size: var(--text-size-18);
   font-weight: bold;
 `;
 
-const BorderLine = styled.hr`
+const Linebar = styled.hr`
   width: 100%;
   border: none;
   background-color: #d9d9d9;
   height: 1px;
   margin-top: 20px;
   margin-bottom: 30px;
+
+  @media screen and (max-width: 991px) {
+    margin-bottom: 20px;
+    margin-bottom: 45px;
+  }
 `;
 
+const ReservationImageBox = styled.figure`
+  width: 100%;
+  display: none;
+  margin-top: 93px;
+  img:first-child {
+    margin-bottom: 20px;
+  }
+
+  @media screen and (max-width: 500px) {
+    display: block;
+  }
+`;
+
+const ReservationImage = styled.img`
+  width: 100%;
+  height: 430px;
+  object-fit: cover;
+
+  @media screen and (max-width: 500px) {
+    height: 300px;
+  }
+`;
+
+const OnedayClassDetailDescBox = styled.article`
+  padding: 40px 25px 0px;
+  line-height: 32px;
+  letter-spacing: -0.06em;
+  display: none;
+  @media screen and (max-width: 500px) {
+    display: block;
+  }
+`;
+
+const OnedayClassDetailDesc = styled.p`
+  font-size: var(--text-size-16);
+  font-family: PretendardRegular;
+`;
 const OnedayClassDetail = () => {
   return (
     <OnedayClassDetialContainer>
@@ -63,10 +130,20 @@ const OnedayClassDetail = () => {
           <ReservationBanner />
           <ReservationFormBox>
             <ReservationFormTitle>예약신청서</ReservationFormTitle>
-            <BorderLine />
+            <Linebar />
             <ReservationForm />
           </ReservationFormBox>
         </ReservationWrapper>
+        <ReservationImageBox>
+          <ReservationImage src={onedayCalssImage1} />
+          <ReservationImage src={onedayCalssImage2} />
+        </ReservationImageBox>
+        <OnedayClassDetailDescBox>
+          <OnedayClassDetailDesc>
+            나에게 가장 잘 어울리는, 오직 나만이 가질 수 있는 그 누구도 없는
+            나만의 향수를 인서리공원에서 만들어보세요.
+          </OnedayClassDetailDesc>
+        </OnedayClassDetailDescBox>
       </OnedayClassDetailWrapper>
     </OnedayClassDetialContainer>
   );
