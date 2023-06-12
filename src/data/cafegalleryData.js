d82c47433a8a5eda49bdc2cff45d14ebd0883015
leejungho9 <9wjdgh9@naver.com>
2023-06-12 17:30:02 +0900

    feat : 클라이언트 측 초기 데이터 설정
    - 초기 데이터 변수 생성해 코드화함

---
+++ src/data/cafegalleryData.js
@@ -0,0 +1,44 @@
+import coffee1 from 'assets/images/cafe/coffee1.png';
+import coffee2 from 'assets/images/cafe/coffee2.png';
+import coffee3 from 'assets/images/cafe/coffee3.png';
+
+const cafegalleryData = [
+  {
+    id: 1,
+    url: `${coffee1}`,
+  },
+  {
+    id: 2,
+    url: `${coffee2}`,
+  },
+  {
+    id: 3,
+    url: `${coffee3}`,
+  },
+  {
+    id: 4,
+    url: `${coffee1}`,
+  },
+  {
+    id: 5,
+    url: `${coffee2}`,
+  },
+  {
+    id: 6,
+    url: `${coffee1}`,
+  },
+  {
+    id: 7,
+    url: `${coffee3}`,
+  },
+  {
+    id: 8,
+    url: `${coffee2}`,
+  },
+  {
+    id: 9,
+    url: `${coffee1}`,
+  },
+];
+
+export default cafegalleryData;
---
+++ src/data/maingalleryData.js
@@ -0,0 +1,35 @@
+import mainImage1 from 'assets/images/main/mainImage1.jpg';
+import mainImage2 from 'assets/images/main/mainImage2.jpg';
+import mainImage3 from 'assets/images/main/mainImage3.jpg';
+import mainImage4 from 'assets/images/main/mainImage4.jpg';
+import mainImage5 from 'assets/images/main/mainImage5.jpg';
+import mainImage6 from 'assets/images/main/mainImage6.jpg';
+
+const maingalleryData = [
+  {
+    id: 1,
+    url: `${mainImage1}`,
+  },
+  {
+    id: 2,
+    url: `${mainImage2}`,
+  },
+  {
+    id: 3,
+    url: `${mainImage3}`,
+  },
+  {
+    id: 4,
+    url: `${mainImage4}`,
+  },
+  {
+    id: 5,
+    url: `${mainImage5}`,
+  },
+  {
+    id: 6,
+    url: `${mainImage6}`,
+  },
+];
+
+export default maingalleryData;
