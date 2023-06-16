79c9c9c11ca23794fcc682738183f544afadcbcd
leejungho9 <9wjdgh9@naver.com>
2023-06-16 17:17:27 +0900

    update : 신청서 유효성 검사를 위한 모달과 예약관련 공지를 위한 모달 적용
    -  Modal, ReservationModal 구분했지만, 합칠 수 있는지 고민해봐야함

--- src/components/Common/Form/ReservationForm.jsx
+++ src/components/Common/Form/ReservationForm.jsx
@@ -3,6 +3,9 @@ import { Form } from 'react-router-dom';
 import styled from 'styled-components';
 import reservationValidate from 'utils/reservationValidate';
 import ReserveButton from '../Button/ReserveButton';
+import useModal from 'hooks/useModal';
+import ReservationModal from '../Modal/ReservationModal';
+import Modal from '../Modal/Modal';
 
 const ClassInfoForm = styled(Form)`
   display: flex;
@@ -131,14 +134,41 @@ const ReservationForm = ({ item }) => {
   const headCountRef = useRef(null);
   const classRequestRef = useRef(null);
   const reservationCheckRef = useRef(null);
+  const [focusRef, setFocusRef] = useState(null);
+  const [
+    errorModalOpen,
+    showErrorModal,
+    closeErrorModal,
+    modalErrorContent,
+    changeErrorContent,
+  ] = useModal();
+
+  const [
+    checkModalOpen,
+    showCheckModal,
+    closeCheckModal,
+    modalCheckContent,
+    changeCheckContent,
+  ] = useModal();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (item.status === 'soldout') {
-      alert('예약이 마감됐습니다.');
+      showCheckModal();
+      changeCheckContent('예약이 마감됐습니다.');
+      return;
+    }
+    if (item.book) {
+      showCheckModal();
+      changeCheckContent('이미 예약이 완료되었습니다. \n 문의 : 061-761-6701');
       return;
     }
     validateReservationForm();
+  };
+
+  //! close callback : input focus()
+  const closeCallBack = (ref) => {
+    ref.current.focus();
   };
 
   const validateReservationForm = () => {
@@ -149,42 +179,55 @@ const ReservationForm = ({ item }) => {
       headCount,
       classRequest,
       reservationCheck,
+      item,
     );
 
     if (!successMessage) {
       if (errorData === 'name') {
-        alert(errorMessage);
-        nameRef.current.focus();
+        showErrorModal();
+        changeErrorContent(errorMessage);
+        setFocusRef(nameRef);
         return;
       }
       if (errorData === 'phone') {
-        alert(errorMessage);
-        phoneRef.current.focus();
+        showErrorModal();
+        changeErrorContent(errorMessage);
+        setFocusRef(phoneRef);
         return;
       }
       if (errorData === 'headCount') {
-        alert(errorMessage);
-        headCountRef.current.focus();
+        showErrorModal();
+        changeErrorContent(errorMessage);
+        setFocusRef(headCountRef);
+
         return;
       }
       if (errorData === 'age') {
-        alert(errorMessage);
-        ageRef.current.focus();
+        showErrorModal();
+        changeErrorContent(errorMessage);
+        setFocusRef(ageRef);
         return;
       }
       if (errorData === 'classRequest') {
-        // alert(errorMessage);
-
-        classRequestRef.current.focus();
+        showErrorModal();
+        changeErrorContent(errorMessage);
+        setFocusRef(classRequestRef);
         classRequestRef.current.selectionStart =
           classRequestRef.current.selectionEnd =
             classRequestRef.current.value.length;
         return;
       }
       if (errorData === 'reservationCheck') {
-        alert(errorMessage);
+        showErrorModal();
+        changeErrorContent(errorMessage);
+        setFocusRef(reservationCheckRef);
         return;
       }
+    }
+
+    if (successMessage) {
+      showErrorModal();
+      changeErrorContent(successMessage);
     }
   };
 
@@ -192,19 +235,30 @@ const ReservationForm = ({ item }) => {
     const { name, value, checked } = event.target;
 
     if (name === 'name') {
-      setName(value);
+      const nameRegex = /[^ㄱ-ㅎ가-힣a-zA-Z]/g;
+      const nameValue = value.replace(nameRegex, '');
+      setName(nameValue);
       return;
     }
     if (name === 'phone') {
-      setPhone(value);
-      return;
-    }
-    if (name === 'age') {
-      setAge(value);
+      const phoneRegex = /[^0-9]/g;
+      const phoneValue = value.replace(phoneRegex, '');
+      setPhone(phoneValue);
       return;
     }
     if (name === 'headCount') {
-      setHeadCount(value);
+      if (value.length <= 2) {
+        const headCountRegex = /[^0-9]/g;
+        const headCountValue = value.replace(headCountRegex, '');
+        setHeadCount(headCountValue);
+      }
+      return;
+    }
+    if (name === 'age') {
+      const ageCountRegex = /[^0-9,]/g;
+      const ageCountValue = value.replace(ageCountRegex, '');
+      setAge(ageCountValue);
+
       return;
     }
     if (name === 'classRequest') {
@@ -216,114 +270,175 @@ const ReservationForm = ({ item }) => {
       return;
     }
   };
-
+  console.log(item);
   return (
-    <ClassInfoForm method="post" onSubmit={handleSubmit}>
-      <ClassInfoBox>
-        <ClassInfoTextBox>
-          <ClassInfoTextLabel>날짜</ClassInfoTextLabel>
-          <ClassInfoTextValue>{item.startDate}</ClassInfoTextValue>
-        </ClassInfoTextBox>
-        <ClassInfoTextBox>
-          <ClassInfoTextLabel>시간</ClassInfoTextLabel>
-          <ClassInfoTextValue>
-            {item.startHour} ~ {item.endHour}
-          </ClassInfoTextValue>
-        </ClassInfoTextBox>
-        <ClassInfoTextBox>
-          <ClassInfoTextLabel>예약마감</ClassInfoTextLabel>
-          <ClassInfoTextValue>{item.endDate}</ClassInfoTextValue>
-        </ClassInfoTextBox>
-        <ClassInfoTextBox>
-          <ClassInfoTextLabel>수강료</ClassInfoTextLabel>
-          <ClassInfoTextValue>{item.price} 원</ClassInfoTextValue>
-        </ClassInfoTextBox>
-        <ClassInfoTextBox>
-          <ClassInfoTextLabel>입금계좌</ClassInfoTextLabel>
-          <ClassInfoTextValue>
-            우리은행 1002-151-887345 이정수
-          </ClassInfoTextValue>
-        </ClassInfoTextBox>
-        <ClassInfoTextBox>
-          <ClassCheckbox
-            type="checkbox"
-            id="check"
-            name="reservationCheck"
-            onChange={handleChange}
-            checked={reservationCheck}
-            ref={reservationCheckRef}
-          />
-          <ClassCheckLable htmlFor="check">
-            위 내용을 모두 확인하였습니다.
-          </ClassCheckLable>
-        </ClassInfoTextBox>
-      </ClassInfoBox>
-      <ClassInfoInputBox>
-        <ClassInfoTextBox>
-          <ClassInfoLabel htmlFor="name">이름</ClassInfoLabel>
-          <ClassInfoInput
-            id="name"
-            name="name"
-            type="text"
-            onChange={handleChange}
-            value={name}
-            ref={nameRef}
-          />
-        </ClassInfoTextBox>
-        <ClassInfoTextBox>
-          <ClassInfoLabel htmlFor="phone">전화번호</ClassInfoLabel>
-          <ClassInfoInput
-            id="phone"
-            name="phone"
-            type="text"
-            onChange={handleChange}
-            value={phone}
-            ref={phoneRef}
-          />
-        </ClassInfoTextBox>
-        <ClassInfoTextBox>
-          <ClassInfoLabel htmlFor="headCount">인원수</ClassInfoLabel>
-          <ClassInfoInput
-            id="headCount"
-            name="headCount"
-            type="text"
-            onChange={handleChange}
-            value={headCount}
-            ref={headCountRef}
-          />
-        </ClassInfoTextBox>
-        <ClassInfoTextBox>
-          <ClassInfoLabel htmlFor="age">체험자 나이</ClassInfoLabel>
-          <ClassInfoInput
-            id="age"
-            name="age"
-            type="text"
-            onChange={handleChange}
-            value={age}
-            ref={ageRef}
-          />
-        </ClassInfoTextBox>
-        <ClassInfoTextBox>
-          <ClassInfoLabel htmlFor="classRequest">요청사항</ClassInfoLabel>
-          <ClassInfoInput
-            id="classRequest"
-            name="classRequest"
-            type="text"
-            onChange={handleChange}
-            value={classRequest}
-            ref={classRequestRef}
-          />
-        </ClassInfoTextBox>
-        <ClassInfoDescBox>
-          <ClassInfoDesc>
-            원데이클래스 참여가능여부 확인되면 핸드폰으로 개별 연락드립니다.
-          </ClassInfoDesc>
-        </ClassInfoDescBox>
-        <ReserveButton type="submit" color={'black'} link={false}>
-          {item.status === 'soldout' ? '예약마감' : '예약하기'}
-        </ReserveButton>
-      </ClassInfoInputBox>
-    </ClassInfoForm>
+    <>
+      <Modal
+        showModal={checkModalOpen}
+        closeModal={closeCheckModal}
+        modalContent={modalCheckContent}
+      />
+      <ReservationModal
+        showModal={errorModalOpen}
+        closeModal={closeErrorModal}
+        closeCallBack={() => {
+          closeCallBack(focusRef);
+        }}
+        modalContent={modalErrorContent}
+      />
+      <ClassInfoForm method="post" onSubmit={handleSubmit}>
+        <ClassInfoBox>
+          <ClassInfoTextBox>
+            <ClassInfoTextLabel>날짜</ClassInfoTextLabel>
+            <ClassInfoTextValue>{item.startDate}</ClassInfoTextValue>
+          </ClassInfoTextBox>
+          <ClassInfoTextBox>
+            <ClassInfoTextLabel>시간</ClassInfoTextLabel>
+            <ClassInfoTextValue>
+              {item.startHour} ~ {item.endHour}
+            </ClassInfoTextValue>
+          </ClassInfoTextBox>
+          <ClassInfoTextBox>
+            <ClassInfoTextLabel>예약마감</ClassInfoTextLabel>
+            <ClassInfoTextValue>{item.endDate}</ClassInfoTextValue>
+          </ClassInfoTextBox>
+          <ClassInfoTextBox>
+            <ClassInfoTextLabel>수강료</ClassInfoTextLabel>
+            <ClassInfoTextValue>{item.price} 원</ClassInfoTextValue>
+          </ClassInfoTextBox>
+          <ClassInfoTextBox>
+            <ClassInfoTextLabel>입금계좌</ClassInfoTextLabel>
+            <ClassInfoTextValue>
+              우리은행 1002-151-887345 이정수
+            </ClassInfoTextValue>
+          </ClassInfoTextBox>
+          <ClassInfoTextBox>
+            {item.book === true ? (
+              <ClassCheckbox
+                type="checkbox"
+                id="check"
+                name="reservationCheck"
+                onChange={handleChange}
+                checked={item.book}
+                ref={reservationCheckRef}
+              />
+            ) : (
+              <ClassCheckbox
+                type="checkbox"
+                id="check"
+                name="reservationCheck"
+                onChange={handleChange}
+                checked={reservationCheck}
+                ref={reservationCheckRef}
+              />
+            )}
+            <ClassCheckLable htmlFor="check">
+              위 내용을 모두 확인하였습니다.
+            </ClassCheckLable>
+          </ClassInfoTextBox>
+        </ClassInfoBox>
+        <ClassInfoInputBox>
+          <ClassInfoTextBox>
+            <ClassInfoLabel htmlFor="name">이름</ClassInfoLabel>
+            {item.book === true ? (
+              <ClassInfoInput readOnly value="이정호" />
+            ) : (
+              <ClassInfoInput
+                id="name"
+                name="name"
+                type="text"
+                onChange={handleChange}
+                value={name}
+                ref={nameRef}
+              />
+            )}
+          </ClassInfoTextBox>
+          <ClassInfoTextBox>
+            <ClassInfoLabel htmlFor="phone">전화번호</ClassInfoLabel>
+            {item.book === true ? (
+              <ClassInfoInput readOnly value={'010-2222-2222'} />
+            ) : (
+              <ClassInfoInput
+                id="phone"
+                name="phone"
+                type="text"
+                onChange={handleChange}
+                value={phone}
+                ref={phoneRef}
+              />
+            )}
+          </ClassInfoTextBox>
+          <ClassInfoTextBox>
+            <ClassInfoLabel htmlFor="headCount">인원수</ClassInfoLabel>
+            {item.book === true ? (
+              <ClassInfoInput readOnly value={4} />
+            ) : (
+              <ClassInfoInput
+                id="headCount"
+                name="headCount"
+                type="text"
+                onChange={handleChange}
+                value={headCount}
+                ref={headCountRef}
+              />
+            )}
+          </ClassInfoTextBox>
+          <ClassInfoTextBox>
+            <ClassInfoLabel htmlFor="age">체험자 나이</ClassInfoLabel>
+            {item.book === true ? (
+              <ClassInfoInput readOnly value={'22,20,23'} />
+            ) : (
+              <ClassInfoInput
+                id="age"
+                name="age"
+                type="text"
+                onChange={handleChange}
+                value={age}
+                ref={ageRef}
+                placeholder="여러 명의 경우, 전부 기입해주세요. 예) 15,16,20"
+              />
+            )}
+          </ClassInfoTextBox>
+          <ClassInfoTextBox>
+            <ClassInfoLabel htmlFor="classRequest">요청사항</ClassInfoLabel>
+            {item.book === true ? (
+              <ClassInfoInput
+                readOnly
+                value={
+                  '4살 애기도 같이 가는데, 참여는 안하고 옆에 있기만 할거에요 '
+                }
+              />
+            ) : (
+              <ClassInfoInput
+                id="classRequest"
+                name="classRequest"
+                type="text"
+                onChange={handleChange}
+                value={classRequest}
+                ref={classRequestRef}
+              />
+            )}
+          </ClassInfoTextBox>
+          <ClassInfoDescBox>
+            <ClassInfoDesc>
+              원데이클래스 참여가능여부 확인되면 핸드폰으로 개별 연락드립니다.
+            </ClassInfoDesc>
+          </ClassInfoDescBox>
+          <ReserveButton
+            type={'submit'}
+            color={'black'}
+            link={false}
+            disabled={item.status === 'soldout' || item.book}
+          >
+            {item.status === 'soldout'
+              ? '예약마감'
+              : item.book
+              ? '예약완료'
+              : '예약하기'}
+          </ReserveButton>
+        </ClassInfoInputBox>
+      </ClassInfoForm>
+    </>
   );
 };
 
