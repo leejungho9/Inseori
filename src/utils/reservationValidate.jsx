31380bd1834b55d30f81eaab1099373d8c20e2d0
leejungho9 <9wjdgh9@naver.com>
2023-06-16 17:20:23 +0900

    update : 원데이클래스 신청서 유효성검사 함수 수정

--- src/utils/reservationValidate.jsx
+++ src/utils/reservationValidate.jsx
@@ -5,24 +5,29 @@ const reservationValidate = (
   headCount,
   classRequest,
   reservationCheck,
+  item,
 ) => {
   let errorMessage = '';
-  // let successMessage = '예약이 완료되었습니다.';
+  let successMessage = '예약이 완료되었습니다. \n 문의 : 061-761-6701';
 
-  //! 이름 = 영문, 한문으로 2자 이상만 가능
-  const nameRegex = /^[가-힣a-zA-Z]{2,}$/;
+  //! 이름 = 영문대소문자, 한글 1자 이상만 가능
+
+  const nameRegex = /^[가-힣a-zA-Z]{1,}$/;
   const isNameValid = nameRegex.test(name);
 
   //! 폰번호 =  11~14자 사이의 숫자만 가능
   const phoneRegex = /^\d{11,14}$/;
   const isPhoneValid = phoneRegex.test(phone);
 
-  //! 나이 =  10자 이하의 숫자만 가능
-  const ageRegex = /^\d{1,3}$/;
+  //! 나이 =  숫자, 쉼표만 가능
+  const ageRegex = /^[0-9,]+[0-9]$/;
   const isAgeValid = ageRegex.test(age);
 
   // ! 인원수 =  1이상의 숫자만 가능
-  const headCountRegex = /^(?:[1-9]|10)$/;
+  const min = item.minHeadCount;
+  const max = item.maxHeadCount;
+  // const headCountRegex = /^(?:[1-9]|10)$/;
+  const headCountRegex = new RegExp(`^(?:[${min}-${max}])$`);
   const isHeadCountValid = headCountRegex.test(headCount);
 
   //! 요청사항  = 30자 이하만 가능
@@ -33,25 +38,28 @@ const reservationValidate = (
   const isReservationCheckValid = reservationCheck;
 
   if (!isNameValid) {
-    errorMessage = '이름이 올바르지 않습니다. 올바른 이름을 입력해주세요.';
+    errorMessage = '이름이 올바르지 않습니다.\n올바른 이름을 입력해주세요.';
     return { errorData: 'name', errorMessage };
   }
-
   if (!isPhoneValid) {
     errorMessage =
-      '전화번호가 올바르지 않습니다. 올바른 전화번호를 입력해주세요.';
+      '전화번호가 올바르지 않습니다.\n올바른 전화번호를 입력해주세요.';
     return { errorData: 'phone', errorMessage };
   }
-
+  if (!isHeadCountValid) {
+    if (headCount < min) {
+      errorMessage = `최소 인원수를 확인해주세요.`;
+      return { errorData: 'headCount', errorMessage };
+    }
+    if (headCount > max) {
+      errorMessage = `최대 인원수를 확인해주세요.`;
+      return { errorData: 'headCount', errorMessage };
+    }
+  }
   if (!isAgeValid) {
     errorMessage =
-      '체험자 나이가 올바르지 않습니다. 올바른 체험자 나이를 입력해주세요.';
+      '체험자 나이가 올바르지 않습니다.\n올바른 체험자 나이를 입력해주세요.';
     return { errorData: 'age', errorMessage };
-  }
-
-  if (!isHeadCountValid) {
-    errorMessage = '최대 인원수를 확인해주세요.';
-    return { errorData: 'headCount', errorMessage };
   }
 
   if (!isClassRequestValid) {
@@ -63,6 +71,7 @@ const reservationValidate = (
     errorMessage = '예약을 완료하시려면 체크 박스에 체크해주세요.';
     return { errorData: 'reservationCheck', errorMessage };
   }
+  return { successMessage };
 };
 
 export default reservationValidate;
