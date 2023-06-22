711d8d4c13fcfe212e0efd723cb99085129ca2dd
leejungho9 <9wjdgh9@naver.com>
2023-06-23 01:30:09 +0900

    update : request 제거 후 getData 함수에 병합

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,22 +2,15 @@ import axios from 'axios';
 
 const { REACT_APP_API } = process.env;
 
-async function request({ url, option }) {
+export const getData = async (path) => {
   try {
-    const response = await axios.get(url, option);
-
+    const response = await axios.get(`${REACT_APP_API}/${path}`, {
+      option: {
+        method: 'GET',
+      },
+    });
     return response.data;
   } catch (error) {
     console.log(error);
   }
-}
-
-export const getData = async (path) => {
-  const response = await request({
-    url: `${REACT_APP_API}/${path}`,
-    option: {
-      method: 'GET',
-    },
-  });
-  return response;
 };
