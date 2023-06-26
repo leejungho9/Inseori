fe3c03da64d696b32d3faa6373d5f53f21056a14
leejungho9 <9wjdgh9@naver.com>
2023-06-26 12:28:14 +0900

    feat : post 요청을 담당하는 postData 함수 생성

--- src/apis/api.js
+++ src/apis/api.js
@@ -4,9 +4,18 @@ const { REACT_APP_API } = process.env;
 
 export const getData = async (path) => {
   try {
-    const response = await axios.get(`${REACT_APP_API}/${path}`, {
-      option: {
-        method: 'GET',
+    const response = await axios.get(`${REACT_APP_API}/${path}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postData = async (path, data) => {
+  try {
+    const response = await axios.post(`${REACT_APP_API}/${path}`, data, {
+      headers: {
+        'Content-Type': 'application/json',
       },
     });
     return response.data;
