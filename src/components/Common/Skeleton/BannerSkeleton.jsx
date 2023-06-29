e6c3657e35bc78d704550cd8eac0cab685d729c1
leejungho9 <9wjdgh9@naver.com>
2023-06-29 19:39:18 +0900

    update : BannerSkeleton size 에 따른 조건부렌더링

--- src/components/Common/Skeleton/BannerSkeleton.jsx
+++ src/components/Common/Skeleton/BannerSkeleton.jsx
@@ -2,19 +2,38 @@ import React from 'react';
 import styled from 'styled-components';
 import SkeletonItem from './SkeletonItem';
 
-const SkeletonBannerWrapper = styled(SkeletonItem)`
+const LargeBannerWrapper = styled(SkeletonItem)`
   padding-top: 50px;
   height: calc(100vh - 277px);
+
+  @media screen and (max-width: 1440px) {
+    height: '540px';
+  }
   @media screen and (max-width: 991px) {
+    height: '480px';
     padding-top: 40px;
     li button:before {
       font-size: 10px;
     }
   }
+  @media screen and (max-width: 500px) {
+    height: '550px';
+  }
 `;
 
-const BannerSkeleton = () => {
-  return <SkeletonBannerWrapper></SkeletonBannerWrapper>;
+const SmallBannerWrapper = styled(SkeletonItem)`
+  width: 100%;
+  height: 35vh;
+  float: left;
+`;
+
+const BannerSkeleton = ({ size }) => {
+  return (
+    <>
+      {size === 'large' && <LargeBannerWrapper />}
+      {size === 'small' && <SmallBannerWrapper />}
+    </>
+  );
 };
 
 export default BannerSkeleton;
