3924cb87747c3b3e7ffb7730096de5eb2e229567
leejungho9 <9wjdgh9@naver.com>
2023-06-30 16:41:46 +0900

    update : BannerSkeleton round 옵션 추가

--- src/components/Common/Skeleton/BannerSkeleton.jsx
+++ src/components/Common/Skeleton/BannerSkeleton.jsx
@@ -7,17 +7,17 @@ const LargeBannerWrapper = styled(SkeletonItem)`
   height: calc(100vh - 277px);
 
   @media screen and (max-width: 1440px) {
-    height: '540px';
+    height: 540px;
   }
   @media screen and (max-width: 991px) {
-    height: '480px';
+    height: 480px;
     padding-top: 40px;
     li button:before {
       font-size: 10px;
     }
   }
   @media screen and (max-width: 500px) {
-    height: '550px';
+    height: 550px;
   }
 `;
 
@@ -27,9 +27,23 @@ const SmallBannerWrapper = styled(SkeletonItem)`
   float: left;
 `;
 
+const RoundBannerWrapper = styled(SkeletonItem)`
+  width: 100%;
+  height: 300px;
+  border-radius: 20px;
+  margin-top: 48px;
+
+  @media screen and (max-width: 991px) {
+    width: 100%;
+    height: 430px;
+    margin-top: 82px;
+  }
+`;
+
 const BannerSkeleton = ({ size }) => {
   return (
     <>
+      {size === 'small and round' && <RoundBannerWrapper />}
       {size === 'large' && <LargeBannerWrapper />}
       {size === 'small' && <SmallBannerWrapper />}
     </>
