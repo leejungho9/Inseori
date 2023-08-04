3f879ed7cebc87376b8ee5d48f2a573ad30e30a8
leejungho9 <9wjdgh9@naver.com>
2023-08-05 01:31:46 +0900

    feat : CarouselWrapper React.memo 활용하여 렌더링 최적화

--- src/components/Common/Carousel/CarouselWrapper.jsx
+++ src/components/Common/Carousel/CarouselWrapper.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styled from 'styled-components';
 import GallerySkeleton from '../Skeleton/GallerySkeleton';
 import Carousel from './Carousel';
@@ -6,40 +7,34 @@ const CarosuelSection = styled.section`
   position: relative;
 `;
 
-const CarouselWrapper = ({
-  slides,
-  width,
-  height,
-  padding,
-  mobilewidth,
-  mobileheight,
-  loading,
-}) => {
-  return (
-    <>
-      {loading ? (
-        <GallerySkeleton
-          length={5}
-          width={width}
-          height={height}
-          padding={padding}
-          mobilewidth={mobilewidth}
-          mobileheight={mobileheight}
-        />
-      ) : (
-        <CarosuelSection>
-          <Carousel
-            slides={slides}
+const CarouselWrapper = React.memo(
+  ({ slides, width, height, padding, mobilewidth, mobileheight, loading }) => {
+    return (
+      <>
+        {loading ? (
+          <GallerySkeleton
+            length={5}
             width={width}
             height={height}
             padding={padding}
             mobilewidth={mobilewidth}
             mobileheight={mobileheight}
           />
-        </CarosuelSection>
-      )}
-    </>
-  );
-};
-
+        ) : (
+          <CarosuelSection>
+            <Carousel
+              slides={slides}
+              width={width}
+              height={height}
+              padding={padding}
+              mobilewidth={mobilewidth}
+              mobileheight={mobileheight}
+            />
+          </CarosuelSection>
+        )}
+      </>
+    );
+  },
+);
+CarouselWrapper.displayName = 'CarouselWrapper';
 export default CarouselWrapper;
