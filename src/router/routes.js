8b5ebf006ae25e7ff4d475c2424b007d1e44ba76
leejungho9 <9wjdgh9@naver.com>
2023-08-16 18:12:17 +0900

    update : let -> const 변경

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,7 +8,7 @@ import GlobalStyle from 'styles/GlobalStyle';
 export function Root() {
   const [topHeaderVisible, setTopHeaderVisible] = useState(true);
   const [leftNavFixed, setLeftNavFixed] = useState(true);
-  let [footerOffsetProps, setFooterOffsetProps] = useState(0);
+  const [footerOffsetProps, setFooterOffsetProps] = useState(0);
   const footerRef = useRef(null);
   const { pathname } = useLocation();
 
@@ -18,14 +18,17 @@ export function Root() {
   };
   //! 상단 헤더와 오른쪽 네브바 스크롤 이벤트
 
+  // 상단 헤더와 네브바 스크롤 이벤트
   useEffect(() => {
     const handleScroll = () => {
-      const footerTop = footerRef.current && footerRef.current.offsetTop; //! 푸터 최상단 위치
-      const windowHeight = window.innerHeight; // ! 브라우저 높이
-      const footerOffset = footerTop - windowHeight; // ! 푸터 위치 - 브라우저 높이
+      const footerTop = footerRef.current && footerRef.current.offsetTop; // 푸터 최상단 위치
+      const windowHeight = window.innerHeight; //  브라우저 높이
+      const footerOffset = footerTop - windowHeight; // 현재 브라우저 높이 - 푸터 최상단 위치
 
+      // 네브바 고정
       if (refs.headerRef && footerRef) {
         if (window.innerWidth > 1024) {
+          // 브라우저 너비가 1024 보다 클 때
           if (window.scrollY >= footerOffset) {
             setLeftNavFixed(false);
           } else {
@@ -34,18 +37,19 @@ export function Root() {
         }
       }
 
-      // ! 상단 헤더 숨기는 이벤트
+      //  상단 헤더 표시
       if (refs.headerRef) {
-        //! 아래 코드가 작동 안해서 임시로 80px 값으로 적어둠
-        // if (window.scrollY > refs.topHeaderRef.current.offsetHeight) {
         if (window.scrollY < 80) {
+          //스크롤 높이가 80 보다 작을 때
           if (window.innerWidth > 991) {
-            setTopHeaderVisible(true);
+            // 브라우저 너비가 991보다 클 때
+            setTopHeaderVisible(true); // 헤더 보이게
             setFooterOffsetProps(0);
           }
         } else {
           if (window.innerWidth > 991) {
-            setTopHeaderVisible(false);
+            //스크롤 높이가 80 보다 크면서 브라우저 너비가 991보다 클 때
+            setTopHeaderVisible(false); //헤더 사라짐
             setFooterOffsetProps(footerOffset);
           }
         }
